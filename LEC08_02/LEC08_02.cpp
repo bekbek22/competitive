2de4242ea439236e1897b9967b151a34e65d55dc
bekbek22 <bekbeknakub@gmail.com>
2023-07-07 22:07:40 +0700

    test

--- LEC08_02/LEC08_02.cpp
+++ LEC08_02/LEC08_02.cpp
@@ -1,46 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n, q;
-    cin >> n >> q;
-    char mode;
-    int left,right,value;
-    int array_ [n];
-    int result[q];
-    int count = 0;
-    int lenght_ = 0;
-    int a;
-    vector<int> arr;
-    for(int i = 0; i < n; i++){
-        cin >> array_[i];
-        // cin >> a;
-        // arr.push_back(a);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    long long N, Q, S, T, K;
+    char c;
+
+    scanf("%ld %ld\n", &N, &Q);
+    long long cows[N];
+
+    for (long long i = 0; i < N; i++)
+    {
+        scanf("%ld\n", &cows[i]);
     }
-    for(int i = 0; i < q; i++){
-        cin >> mode;
-        if(mode == 'C'){
-            cin >> left >> right >> value;
-            for(int j = left-1; j < right; j++){
-                if(array_[j] <= value){
-                    count += 1;
-                }
-            }
-            result[lenght_] = count;
-            lenght_ += 1;
-            count = 0;
+
+    for (long long i = 0; i < Q; i++)
+    {
+
+        scanf("%c", &c);
+
+        if (c == 'M')
+        {
+            long long i, X;
+            scanf("%ld %ld\n", &i, &X);
+            cows[i - 1] = X;
         }
-        else if(mode == 'M'){
-            cin >> right >> value;
-            array_[right-1] = value;
+        else
+        {
+
+            scanf("%ld %ld %ld\n", &S, &T, &K);
+
+            long long count = count_if(cows + S - 1, cows + T, [K](long long x)
+                                 { return x <= K; });
+
+            printf("%ld\n", count);
         }
-    }
-    for(int i = 0; i < lenght_; i++){
-        cout << result[i] << endl;
     }
 
     return 0;
-
 }
